a8d3d3ced53936a3d819cf6fedfc2b2f051d233c
realyutou <ssps9430@gmail.com>
2023-09-19 16:36:22 +0800

    feat: add signin page

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // Include packages and define related variables
 const express = require('express')
+const { engine } = require('express-handlebars')
 
 const routes = require('./routes')
 
 const port = 3000
 const app = express()
+
+// Set express-handlebars
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+// Set body-parser
+app.use(express.urlencoded({ extended: true }))
 
 // Set routes
 app.use(routes)
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 // Include packages and define related variables
 const express = require('express')
 
+const userController = require('../controllers/user-controller')
+
 const router = express.Router()
+
+// Signin page
+router.get('/signin', userController.signInPage)
 
 // Homepage
 router.get('/', (req, res) => {
